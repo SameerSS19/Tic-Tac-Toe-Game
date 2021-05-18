af1725a35c0c93a6462a9162b6b7c5d9690d3c1f
SameerSS19 <sk3090019@gmail.com>
2021-05-18 08:49:36 +0530

    Create a board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,8 +1,19 @@
 package com.tictactoy;
 
-public class TicTacToeGame {
-	public static void main(String[] args) {
-	System.out.println("Welcome to Tic Tac Toe Game");
+public class TicTacToeGame 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Tic Tac Toe Game");
+		char[] board = createBoard();
 	}
-
+	public static char[] createBoard()
+	{
+		char[] board = new char[10];
+		for(int index =0; index < board.length; index++)
+		{
+			board[index] = ' ';
+		}
+		return board;
+	}
 }
