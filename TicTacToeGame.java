a4feb6937ab5a3b3d52fc1b3a69c1060086b20e7
SameerSS19 <sk3090019@gmail.com>
2021-05-20 23:25:28 +0530

    Check win or tie

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -24,6 +24,8 @@ public class TicTacToeGame
 		getUserMove(board,UserLetter);
 		showBoard(board);
 		toss();
+		char win =winnerCheck(board);
+		System.out.println("Winner is " + win);
 	}
 	public static void toss()
     {
@@ -90,6 +92,62 @@ public class TicTacToeGame
 			board[index] = ' ';
 		}
 		return board;
+	}
+	public static char winnerCheck(char[] board)
+	{
+		for( int i = 1; i < 3; i++ )  
+	    {
+			for(int j = 1;j <= i; j = j+3)
+			{
+				if( ( board[j] == 'X') && ( board[i+j] == 'X') && ( board[i+j+1] == 'X' ) )
+				{
+					return 'X';
+				}
+				if( ( board[j] == 'O' ) && ( board[i+j] == 'O' ) && ( board[i+j+1] == 'O' ) )
+				{
+					return 'O';
+				}
+			}
+	    }
+	    for( int i = 1; i < 3; i++ ) 
+	    {
+	    	for(int j = 1;j <= i; j++)
+	    	{
+	    		if( ( board[j] == 'X' ) && ( board[j+i+2] == 'X' ) && ( board[j+i+5] == 'X' ) )
+	    		{
+	    			return 'X';
+	    		}
+	        
+	    		if( ( board[j] == 'O' ) && ( board[j+i+2] == 'O' ) && ( board[j+i+5] == 'O' ) )
+	    		{
+	    			return 'O';
+	    		}
+	    	}
+	    }
+	    // For Diagonal
+	    
+	    if( ( board[1] == 'X' ) &&  ( board[5] == 'X' ) &&  ( board[9] == 'X' ) )
+	        return 'X';
+	    if( ( board[3] == 'X' ) &&  ( board[5] == 'X' ) &&  ( board[7] == 'X' ) )
+	    	return 'X';
+	    if( ( board[1] == 'O' ) &&  ( board[5] == 'O' ) &&  ( board[9] == 'O' ) )
+	    	return 'O';
+	    if( ( board[3] == 'O' ) &&  ( board[5] == 'O' ) &&  ( board[7] == 'O' ) )
+	    	return 'O';
+	    
+	    for( int i = 1; i < board.length; i++ )
+	    {
+	    	int  cell=0;
+	    	if(board[i]!=' ')
+	    	{
+	    		cell++;
+	    	}
+	    	if(cell==9)
+	    	{
+	    	System.out.println("Match is tie");
+	    	}
+	    }
+		return 0;
 	}
 }
 
